13218b2400375c1e386c3e7b1d49cf02919c0f01
CAUAN SANTANA SILVA <cauan.ssilva@senacsp.edu.br>
2022-08-08 16:18:32 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from ControlPessoa import ControlPessoa
+
+
+if __name__ == "__main__":
+    controle = ControlPessoa()
+    controle.operacao()#Chamando o método de alteração de dados da classe control
+
